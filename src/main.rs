3b6c9f8ac0197208a513b30155ccdd52c45b64db
k <k>
2021-10-08 21:21:12 +0200

    both async

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::io::{stdout, Write};
 use std::str::from_utf8;
 use json;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use tokio::task::spawn_blocking;
+use std::sync::{Mutex, Arc};
 
 const NEWSITEMS: usize = 30;
 
@@ -13,7 +15,7 @@ const NEWSITEMS: usize = 30;
 struct NewsItem {
     id: u32,
     title: String,
-    url: String,
+    url: Option<String>,
     score: u32,
     seen: Instant,
 }
@@ -28,20 +30,41 @@ fn main() {
   }
 }*/
 
-async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello World")))
+async fn handle(_req: Request<Body>, news: Arc<Mutex<Vec<NewsItem>>>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from(format!("{:?}",*news.lock().unwrap()))))
 }
 
 #[tokio::main]
 async fn main() {
+    let news = Arc::new(Mutex::new(spawn_blocking(|| update_news(&[])).await.unwrap()));
+    let news2 = news.clone();
+    
     // Construct our SocketAddr to listen on...
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     // And a MakeService to handle each connection...
-    let make_service = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(handle))
+    let make_service = make_service_fn(move |_conn| {
+        let news2 = news.clone();
+        async move {
+            let news3 = news2.clone();
+            Ok::<_, Infallible>(service_fn(move |req| handle(req, news3.clone())))
+        }
     });
 
+    let join_handle = tokio::spawn(async move {
+        loop {
+            tokio::time::sleep(Duration::from_secs(5*60));
+            let news3 = news2.clone();
+            let new_news = spawn_blocking(move || {
+                let mut news_guard = news3.lock().unwrap();
+                update_news(&*news_guard)
+            }).await.unwrap();
+            let mut news_guard = news2.lock().unwrap();
+            *news_guard = new_news;
+    
+        }
+    });
+    
     // Then bind and serve...
     let server = Server::bind(&addr).serve(make_service);
 
@@ -54,6 +77,8 @@ async fn main() {
 
 fn update_news(old_news : &[NewsItem]) -> Vec<NewsItem> {
   let body = reqwest::blocking::get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty").unwrap();
+    
+    
 
   let mut list: Vec<u32> = body.json().unwrap();
     
@@ -66,10 +91,11 @@ fn update_news(old_news : &[NewsItem]) -> Vec<NewsItem> {
         json::JsonValue::Object(obj) => obj,
         _ => panic!("expected json object"),
       };
+    
       let mut item = NewsItem {
         id: entry_json_obj["id"].as_u32().unwrap(),
         title: entry_json_obj["title"].as_str().unwrap().to_owned(),
-        url: entry_json_obj["url"].as_str().unwrap().to_owned(),
+        url: entry_json_obj.get("url").map(|u| u.as_str().unwrap().to_owned()),
         score: entry_json_obj["score"].as_u32().unwrap(),
         seen: Instant::now()
       };
@@ -84,4 +110,3 @@ fn update_news(old_news : &[NewsItem]) -> Vec<NewsItem> {
   
   newslist
 }
-
